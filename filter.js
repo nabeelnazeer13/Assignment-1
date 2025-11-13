789351b9c16683ceac5830a306e0086bc260d7e2
theskylla <theskylla@gmail.com>
2025-11-13 21:40:43 +0100

    updated input of type  to addEventListener

--- filter.js
+++ filter.js
@@ -1,13 +1,27 @@
-//toggle online/offline checkbox apperence
-function toggleCheckbox() {
-    var checkBox = document.getElementById("myCheck");
-    var text = document.getElementById("text");
-    if (checkBox.checked == true){
-        text.style.display = "block";
+//adds to array when selected using addEventlistener
+const selectedTypes = [];
+
+const onlineBox = document.querySelector('.online');
+const offlineBox = document.querySelector('.offline');
+
+function checkBox(event) {
+    const value = event.target.value;
+
+    if (event.target.checked) {
+      // Add the value if checked
+      selectedTypes.push(value);
     } else {
-       text.style.display = "none";
-    }
+      // Remove it if unchecked
+      const index = selectedTypes.indexOf(value);
+      if (index > -1) {
+        selectedTypes.splice(index, 1);
+      }
+
+    } console.log(selectedTypes) 
 }
+  onlineBox.addEventListener('change', checkBox);
+  offlineBox.addEventListener('change', checkBox);
+
 
 //toogle tag state, added to selectedTags array if checked
 let selectedTags = [];
