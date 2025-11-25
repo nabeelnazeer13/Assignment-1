4ee1ce4f98c74ceedd3cb4a7acffeccdf2fc5b7a
elinmolindr <elinmolinder1@gmail.com>
2025-11-25 09:24:19 +0100

    More comments to main.js

--- main.js
+++ main.js
@@ -41,9 +41,11 @@ function createChallengeLi(ch) {
     const emptyStars = '☆'.repeat(empty);
     const typeText = type === 'onsite' ? '(on-site)' : '(networked)';
 
-    //Creates a li-element med HTML for challenge
+    //Creates a li-element to hold a challenge and assigns a CSS class for styling the list
     const li = document.createElement('li');
     li.className = 'challenges__listItem';
+    
+    //Fill the <li>-element with the HTML structure for a challenge
     li.innerHTML = `
     <article class="challenge">
      <div class="challenge__imageWrapper">
@@ -58,6 +60,7 @@ function createChallengeLi(ch) {
         <h3 class="challenge__title">${title} ${typeText}</h3>
         <span class="challenge__size">${minParticipants}–${maxParticipants} participants</span>
 
+<!--Shows a specific icon if the type is online/onsite-->
         ${type === "online" ? `
           <span class="challenge__icon"> 
           <svg height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 85.356 85.356" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path style="fill:#010002;" d="M77.45,57.695v-3.16V35.567V16.599c0-1.736-1.421-3.16-3.16-3.16H11.066 c-1.739,0-3.164,1.417-3.164,3.16v18.968v18.968v3.16L0,67.179c0,2.613,2.122,4.738,4.742,4.738h75.872 c2.616,0,4.742-2.126,4.742-4.738L77.45,57.695z M49.005,70.342H36.358c-0.437,0-0.791-0.351-0.791-0.791 c0-0.44,0.354-0.791,0.791-0.791h12.648c0.433,0,0.791,0.351,0.791,0.791C49.785,69.992,49.438,70.342,49.005,70.342z M29.647,67.182l2.412-2.895h21.233l2.416,2.895H29.647z M73.001,52.104c0,1.525-1.242,2.759-2.756,2.759H15.11 c-1.514,0-2.756-1.245-2.756-2.759V19.036c0-1.525,1.242-2.759,2.756-2.759h55.136c1.514,0,2.756,1.242,2.756,2.759 C73.001,19.036,73.001,52.104,73.001,52.104z"></path> </g> </g></svg>             
@@ -70,7 +73,7 @@ function createChallengeLi(ch) {
 
       <p class="challenge__description">${description}</p>
 
-      <!-- valfritt: visa etiketter om du har stil för dem--> 
+<!--Checks if there are any labels in the labels array. Yes= show tags. No= no output -->
       ${labels.length ? `<div class="challenge__labels">${labels.map(l => `<span class="tags">#${l}</span>`).join(' ')}</div>` : ''}
         
       <div class="challenge__buttonWrapper">
