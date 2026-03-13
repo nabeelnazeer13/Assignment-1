bd0cd117a1ade05a224de8060bdb1ce5f641b301
nabeelnazeer13 <nabeel.nazeer13@gmail.com>
2026-03-13 03:42:23 +0100

    add http server script

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    baseUrl: "http://localhost:5500/Assignment-1/" ,
+    baseUrl: "http://localhost:5500" ,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
